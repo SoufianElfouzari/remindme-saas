5309c8b8a012ce6484b4b2a58e0b14e003099cb1
Soufian El-Fouzari <elfouzari.soufian@gmail.com>
2026-02-21 20:47:33 +0100

    Improved: Conntect to login screen

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:remindme/screens/login_screen.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -6,7 +7,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Placeholder(),
+      home: LoginScreen(),
     );
   }
 }
