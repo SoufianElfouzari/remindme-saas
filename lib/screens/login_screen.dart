1b3dcc59a1844eb99cb412db8625d5aed820c9aa
Soufian El-Fouzari <elfouzari.soufian@gmail.com>
2026-02-22 00:19:22 +0100

    feat: Add login formular

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:remindme/constants/colors.dart';
 import 'package:remindme/constants/text_styles.dart';
+import 'package:remindme/widgets/common/button.dart';
 import 'package:remindme/widgets/common/text_field.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -8,30 +9,50 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: AppColors.background,
       body: Row(
         children: [
           // TODO Left Side: Login Form (Email, Password, Login Button etc.)
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Login', style: AppTextStyles.h1,),
-                SizedBox(height: 20),
-                // TODO Email TextField
-                // TODO Password TextField
-                // TODO Login Button
+          SizedBox(
+            width: screenWidth * 0.4, // 40% vom Screen,
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(40, 10, 40, 20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Login', style: AppTextStyles.h1),
+                  SizedBox(height: 20),
+                  AppTextField(
+                    label: 'Email eingeben',
+                    controller:
+                        TextEditingController(), //Todo: Placeholder Controller ersetzen
+                  ),
 
-              ],
+                  const SizedBox(height: 22),
+
+                  AppTextField(
+                    label: 'Passwort eingeben',
+                    controller:
+                        TextEditingController(), //Todo: Placeholder Controller ersetzen
+                  ),
+                  // TODO Login Button
+                  const SizedBox(height: 30),
+                  AppButton(
+                    buttonColor: AppColors.primary,
+                    buttonRadius: BorderRadius.circular(10),
+                    buttonHeight: 35,
+                    buttonText: const Text('Login', style: AppTextStyles.button),
+                  ),
+                ],
+              ),
             ),
           ),
           // TODO Right Side: Image.
-          Expanded(
-            child: Placeholder(),
-          ),
+          Expanded(child: Placeholder()),
         ],
-      )
+      ),
     );
   }
 }
