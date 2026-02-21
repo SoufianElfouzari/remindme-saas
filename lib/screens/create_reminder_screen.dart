87c65e132b6b56ecbf48bbb94c7d6c5adc8f88ef
Soufian El-Fouzari <elfouzari.soufian@gmail.com>
2026-02-21 20:18:47 +0100

    Add Create Reminder Screen inital structure

---
+++ lib/screens/create_reminder_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CreateReminderScreen extends StatelessWidget {
+  const CreateReminderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
