23ac6614749c648da677f8fe86e15d23bbe924c8
Soufian El-Fouzari <elfouzari.soufian@gmail.com>
2026-02-22 20:29:58 +0100

    feat: add appwrite initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:remindme/app.dart';
+import 'package:remindme/services/appwrite_client.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  AppwriteClient.init();
   runApp(const App());
 }
