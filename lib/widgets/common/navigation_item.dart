1094cb571e888faa9954049180813cbce0dab67f
Soufian El-Fouzari <elfouzari.soufian@gmail.com>
2026-02-24 20:32:24 +0100

    feat: add initial structure

---
+++ lib/widgets/common/navigation_item.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class NavigationItem extends StatelessWidget {
+  const NavigationItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Row(
+        children: [
+          //Todo: Icon
+          //Todo: Text
+          //Todo: Line if active
+        ],
+      ),
+    );
+  }
+}
