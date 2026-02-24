7f5a293d3149233ffeee9ce09838267336dbe9a0
Soufian El-Fouzari <elfouzari.soufian@gmail.com>
2026-02-24 17:43:31 +0100

    feat: add login check inkl. error handling

--- lib/app.dart
+++ lib/app.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:remindme/screens/login_screen.dart';
+import 'package:remindme/screens/shell_page.dart';
+import 'package:remindme/utils/check_login.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: LoginScreen(),
+    return FutureBuilder<bool>(
+      future: isLoggedIn(),
+      builder: (context, snapshot) {
+        // This is Returend while checking login status
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const MaterialApp(
+            home: Scaffold(body: Center(child: CircularProgressIndicator())),
+          );
+        }
+
+       if (snapshot.hasError) {
+          return const MaterialApp(home: LoginScreen());
+        }
+
+        if (snapshot.data == true) {
+          return const MaterialApp(home: ShellPage());
+        }
+
+        // Default: Not logged in
+        return const MaterialApp(home: LoginScreen());
+      },
     );
   }
 }
