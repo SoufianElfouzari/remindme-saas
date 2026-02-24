ec6453e0d939eb42c68a6756368f5e671a9440c9
Soufian El-Fouzari <elfouzari.soufian@gmail.com>
2026-02-24 21:27:52 +0100

    feat: replace static dashboard page with dynamic pages

--- lib/screens/shell_page.dart
+++ lib/screens/shell_page.dart
@@ -1,25 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:remindme/screens/dashboard_screen.dart';
+import 'package:remindme/screens/reminders_screen.dart';
 import 'package:remindme/widgets/common/sidebar.dart';
 
-class ShellPage extends StatelessWidget {
+class ShellPage extends StatefulWidget {
   const ShellPage({super.key});
 
   @override
+  State<ShellPage> createState() => _ShellPageState();
+}
+
+class _ShellPageState extends State<ShellPage> {
+  int selectedIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         //Todo: Header
         children: [
           Row(
             children: [
-              Sidebar(),
+              Sidebar(
+                selectedIndex: selectedIndex,
+                onItemSelected: (index) {
+                  setState(() {
+                    selectedIndex = index;
+                  });
+                },
+              ),
+
               //Todo: Page
-              DashboardScreen(), //Todo: Replace Dashboard with real dynamic page
+              Expanded(
+                child: _getPage(),
+              ),
             ],
           ),
         ],
       ),
     );
+  }
+
+   Widget _getPage() {
+    switch (selectedIndex) {
+      case 0:
+        return const DashboardScreen();
+      case 1:
+        return const RemindersScreen();
+      case 2:
+        return const Center(child: Text("Third Page"));
+      default:
+        return const DashboardScreen();
+    }
   }
 }
