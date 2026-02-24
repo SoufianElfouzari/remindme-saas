f4d4b07e9dc9dc0faeed647a667950def38bc367
Soufian El-Fouzari <elfouzari.soufian@gmail.com>
2026-02-24 20:02:12 +0100

    impr: add inital structure

---
+++ lib/widgets/common/sidebar.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Sidebar extends StatelessWidget {
+  const Sidebar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        //Todo: Logo
+        //Todo: Navigation Items
+        //Todo: Help & Support Button
+        //Todo: Logout Button
+      ],
+    );
+  }
+}
