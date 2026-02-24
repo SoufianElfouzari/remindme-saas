4e2be19d5d0c6a0ba67b151db76ae194f224e1ec
Soufian El-Fouzari <elfouzari.soufian@gmail.com>
2026-02-24 20:10:53 +0100

    impr: set fixed size for sidebar

--- lib/widgets/common/sidebar.dart
+++ lib/widgets/common/sidebar.dart
@@ -5,18 +5,23 @@ class Sidebar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        //Todo: Logo
-        SizedBox(
-          height: 100,
-          width: double.infinity,
-          child: Placeholder(),
-        )
-        //Todo: Navigation Items
-        //Todo: Help & Support Button
-        //Todo: Logout Button
-      ],
+    return SizedBox(
+      width: 275,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(30, 50, 30, 50),
+        child: const Column(
+          children: [
+            SizedBox(
+              height: 100,
+              width: double.infinity,
+              child: Placeholder(),
+            )
+            //Todo: Navigation Items
+            //Todo: Help & Support Button
+            //Todo: Logout Button
+          ],
+        ),
+      ),
     );
   }
 }
