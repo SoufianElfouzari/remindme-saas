470d79efe9e91e4d06174d4e69154bb34574fe69
Soufian El-Fouzari <elfouzari.soufian@gmail.com>
2026-02-24 17:42:42 +0100

    impr: remove useless login check

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:remindme/app.dart';
-import 'package:remindme/screens/dashboard_screen.dart';
 import 'package:remindme/services/appwrite_client.dart';
-import 'package:remindme/utils/check_login.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   AppwriteClient.init();
-  isLoggedIn().then((isLoggedIn) {
-    runApp(isLoggedIn ? const DashboardScreen() : const App());
-  });
+  runApp(const App());
 }
