56bdcd13a5168b32934c072f0d10f6af6ef8a37c
Wierss Programm <elfouzari.soufian@gmail.com>
2026-02-25 22:58:19 +0100

    impr: replace into theme-constants folder

---
+++ lib/constants/theme/colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  //* BRAND COLOR
+  static const Color primary = Color.fromARGB(255, 64, 111, 212);
+
+  //* BACKGROUND COLORS
+  static const Color background = Color(0xFFF9FAFB);
+  static const Color surface = Color(0xFFFFFFFF);
+
+  //* TEXT COLORS
+  static const Color textPrimary = Color(0xFF111827);
+  static const Color textSecondary = Color(0xFF6B7280);
+  static const Color textInverse = Color(0xFFFFFFFF);
+
+  //* BORDER COLOR
+  static const Color border = Color(0xFFE5E7EB);
+
+  //* STATUS COLORS
+  static const Color success = Color.fromARGB(255, 46, 216, 162);
+  static const Color error = Color.fromARGB(255, 218, 80, 80);
+
+  //* STATES COLOR
+  static const Color disabled = Color(0xFF9CA3AF);
+}
