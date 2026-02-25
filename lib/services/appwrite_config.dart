cdcb2e075d929581bdd5720925599278c8dcda33
Wierss Programm <elfouzari.soufian@gmail.com>
2026-02-25 22:59:11 +0100

    impr: remove replaced files

--- lib/services/appwrite_config.dart
+++
@@ -1,7 +0,0 @@
-class AppwriteConfig {
-  static const String endpoint = 'https://cloud.appwrite.io/v1';
-  static const String projectId = 'YOUR_PROJECT_ID';
-
-  static const String databaseId = 'YOUR_DATABASE_ID';
-  static const String remindersCollectionId = 'YOUR_REMINDERS_COLLECTION_ID';
-}
